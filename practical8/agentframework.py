806482f15c627c6254cfab06538509c7e0cd81db
Krasen Samardzhiev <krasensam@gmail.com>
2017-12-12 15:51:35 +0000

    comments

--- practical8/agentframework.py
+++ practical8/agentframework.py
@@ -2,6 +2,8 @@
 """
 Created on Wed Sep 20 09:54:47 2017
 
+A class file that holds the Agent class.
+
 @author: gyks
 """
 import random
@@ -10,16 +12,21 @@ import math
 class Agent():
     
     def __init__(self, environment, agents, c, s):
+
+        # initialize the agent at a random location
         self.x = random.randint(0,100);
         self.y = random.randint(0,100)
+        self.store = 0
+
+        # save the rest of the agent parameters
         self.environment = environment
         self.agents = agents
-        self.store = 0
         self.color = c
         self.shape = s
         
     def move(self):
         
+        # move the agent by to a random x and y location
         if random.random() < 0.5:
             self.x = (self.x + 1) % 99
         else:
@@ -30,15 +37,21 @@ class Agent():
         else:
             self.y = (self.y - 1) % 99
             
-    def eat(self): # can you make it eat what is left?
+
+    def eat(self):
+        # if there is enough food in the enviroment each it
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10
-			
+		
+
     def distance_with(self, other):
+        # return the eucledian distance to the other agent
         return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
     
+
     def share_with_neighbours(self, neighbourhood):
+        # share half of the the food with any agent in the neighbourhood
         for agent in self.agents:
             if self.distance_with(agent) <= neighbourhood:
                 shared_store = (self.store + agent.store) / 2
--- practical8/model.py
+++ practical8/model.py
@@ -1,23 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 09:54:47 2017
+
+The script that reads in the environment and runs the model.
+
+@author: gyks
+"""
+#import everything required
 import matplotlib.pyplot
 import agentframework
 import csv
 import random
 from itertools import cycle
 
-matplotlib.pyplot.ion()
+# setup the model parameters
 num_of_agents = 10
-num_of_iterations = 400
+num_of_iterations = 200
 MIN_X ,MIN_Y = 0, 0
 MAX_X, MAX_Y = 100, 100
-neighbourhood = 20
+neighbourhood = 10
 DATAFILE = 'in.txt'
+# cycle through the colors and shapes to create a unique combination
+# of color and shape for up to 42 agents
 cycol = cycle('bgrcmk')
 cyshape = cycle('oD|ps2*')
-
 
 environment = []
 agents = []
 
+# read in the data
 with open(DATAFILE) as csvfile:
 	data = csv.reader(csvfile, delimiter=',')
 	for row in data:
@@ -29,17 +40,21 @@ with open(DATAFILE) as csvfile:
 c = next(cycol)
 s = next(cyshape)
 
+# create each agent
 for i in range(num_of_agents):
     agents.append(agentframework.Agent(environment, agents, c, s))
     c = next(cycol)
     if c == 'k':
         s = next(cyshape)
 
+# plot initial frame
 matplotlib.pyplot.ylim(0, 100)
 matplotlib.pyplot.xlim(0, 100)
 matplotlib.pyplot.imshow(environment)
 
+# simulate the agent based model
 for n in range(num_of_iterations):
+    # shuffle the agents and perform their actions
     random.shuffle(agents)
     for agent in agents:
 	    agent.move()
@@ -49,8 +64,16 @@ for n in range(num_of_iterations):
     matplotlib.pyplot.xlim(0, 100)
     matplotlib.pyplot.imshow(environment)
 	
+    # plot the agents on the frame
     for a in agents:
         matplotlib.pyplot.plot(a.x, a.y, a.shape, markersize=a.store/100, color=a.color)
+    # draw the frame
     matplotlib.pyplot.draw()
     matplotlib.pyplot.pause(0.001)
+    # clear the frame
     matplotlib.pyplot.clf()
+
+matplotlib.pyplot.imshow(environment)
+for a in agents:
+    matplotlib.pyplot.plot(a.x, a.y, a.shape, markersize=a.store/100, color=a.color)
+matplotlib.pyplot.show()
